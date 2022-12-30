6037ec20569d4905ad03cec1c3c8e9c8bf38f643
JongminChoi <joeychoi7624@gmail.com>
2022-12-30 23:02:54 +0900

    [ADD] user profile get(Query) & edit(Mutation)

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -6,7 +6,9 @@ import {
   CreateAccountInput,
   CreateAccountOutput,
 } from './dtos/create-account.dto';
+import { EditProfileInput, EditProfileOutput } from './dtos/edit-profile.dto';
 import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { UserProfileInput, UserProfileOutput } from './dtos/user-profile.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './users.service';
 
@@ -47,7 +49,48 @@ export class UsersResolver {
 
   @Query(() => User)
   @UseGuards(AuthGuard)
-  myProfile(@AuthUser() authUser: User) {
+  me(@AuthUser() authUser: User) {
     return authUser;
+  }
+
+  @UseGuards(AuthGuard)
+  @Query(() => User)
+  async userProfile(
+    @Args() userProfileInput: UserProfileInput,
+  ): Promise<UserProfileOutput> {
+    try {
+      const user = await this.usersService.findById(userProfileInput.userId);
+      if (!user) {
+        throw Error();
+      }
+      return {
+        success: true,
+        user,
+      };
+    } catch (error) {
+      return {
+        error: 'User Not Found',
+        success: false,
+      };
+    }
+  }
+
+  @UseGuards(AuthGuard)
+  @Mutation(() => EditProfileOutput)
+  async editProfile(
+    @AuthUser() authUser: User,
+    @Args('input') editProfileInput: EditProfileInput,
+  ): Promise<EditProfileOutput> {
+    try {
+      await this.usersService.editProfile(authUser.id, editProfileInput);
+      return {
+        success: true,
+      };
+    } catch (error) {
+      return {
+        success: false,
+        error,
+      };
+    }
   }
 }
