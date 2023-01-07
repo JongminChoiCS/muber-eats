dabf22e0c247c2aea10103fe03b046f99d3b4f4b
JongminChoi <joeychoi7624@gmail.com>
2023-01-08 01:23:46 +0900

    [TEST] user e2e test setup

--- muber-eats-backend/src/app.module.ts
+++ muber-eats-backend/src/app.module.ts
@@ -25,7 +25,7 @@ import { MailModule } from './mail/mail.module';
           : '.env.test.local',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        NODE_ENV: Joi.string().valid('dev', 'prod', 'test').required(),
         DB_HOST: Joi.string().required(),
         DB_PORT: Joi.string().required(),
         DB_USERNAME: Joi.string().required(),
@@ -43,7 +43,8 @@ import { MailModule } from './mail/mail.module';
       database: process.env.DB_NAME,
       synchronize: process.env.NODE_ENV !== 'prod',
       entities: [User, Verification],
-      logging: true,
+      logging:
+        process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
